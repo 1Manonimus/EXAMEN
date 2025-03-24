4fd10530b3b5863a61141430b725c4336e72c4c9
Elver Galarga <mangoancestral@gmail.com>
2025-03-24 16:39:24 -0600

    Create EJERCICIO 1.c

---
+++ EJERCICIO 1.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+int n;
+
+printf ("ingrese el valor;\n");
+
+scanf ("%u",&n);
+{
+if (n>=1)
+{
+    printf ("el valor es positivo");
+}
+else 
+{
+if (n<1 , n !=0)
+    {
+         printf ("el valor es negativo");
+    }
+    else
+        {
+            printf ("el valor es cero");
+        }
+    
+}
+}
+    return 0;
+}
