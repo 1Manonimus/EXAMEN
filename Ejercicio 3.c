2c10fff790b67f7d3ad5d3583b084efa0f453ce4
Elver Galarga <mangoancestral@gmail.com>
2025-03-24 19:03:34 -0600

    Create Ejercicio 3.c

---
+++ Ejercicio 3.c
@@ -0,0 +1,26 @@
+int main()
+{
+int sum = 0;
+int i=0;
+int a;
+int b;
+
+
+printf ("ingresar los valores, esté se mostrara hasta que la suma supere 78500\n");
+ printf ("ingrese los número que quiere sumar;\n");
+
+{  
+    while(sum<78500)
+    {
+        scanf("%u",&a);
+    
+        sum=sum+a;
+    }
+    {
+printf ("la suma es %d\n",sum);
+    }
+}
+
+
+    return 0;
+}
