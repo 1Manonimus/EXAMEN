4c8865b9c92cdb206c24f12f43dc1e2a82325a70
Elver Galarga <mangoancestral@gmail.com>
2025-03-24 18:16:44 -0600

    Update Ejercicio 2.c

--- Ejercicio 2.c
+++ Ejercicio 2.c
@@ -1,1 +1,34 @@
+#include <stdio.h>
 
+int main()
+{
+   int i=10;
+   int n;
+       
+{
+    while(i>10,i--)
+    {
+printf("ingrese un valor;\n");
+scanf("%u",&n);
+    {
+        if (n<10)
+        {
+            printf("el valor es menor que 10\n");
+        }
+        else
+        {
+            if(n<=100)
+            {
+                printf("el valor esta entre 10 y 100\n");
+            }
+            else
+            {
+                printf("el valor es mayor que 100\n");
+            }
+        }
+    }
+    }
+}
+
+    return 0;
+}
