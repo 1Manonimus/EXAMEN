bbc864d35e3690da1465a7318c7797f6eb3b0026
Elver Galarga <mangoancestral@gmail.com>
2025-03-26 18:56:06 -0600

    Ejercicio 6

--- Ejercicio 6  incompleto.c
+++ Ejercicio 6  incompleto.c
@@ -2,35 +2,47 @@
 
 int main()
 {
-int sum=0;
-int pro=0;
-int i=1;
-int y=1;
-int x=1;
-int a;
-
-printf("Los 10 valores que se den, se obtendra su promedio(solo se contara en el intervalo de 5-2500)\n");
+  int sum=0;
+  int pro=0;
+  int i=1;
+  int y=0;
+  int a;
+  
+ printf("Ingresa los valores a los que quiere obtener promedio (solo se tomaran en cuenta los números comprendidos entre el 5 y el 2500\n");
 {
 while(i<=10)
 {
-printf("introduzca el valor %d\n",i);
-scanf("%d",&a);
-{
-    if(a>=5 , a<=2500)
+    printf("ingresa el valor %d\n",i);
+    scanf("%d",&a);
+    if(a<5)
     {
-        sum=sum+a;
-        y=y+1;
+        sum=sum+0;
     }
     else
     {
-        x=x+1;
+        if(a<=2500)
+        {
+            sum=sum+a;
+            y=y+1;
+        }
+        else
+        {
+            sum=sum+0;
+        }
     }
-}
 i=i+1;
 }
 }
-pro=sum/y;
-printf("el promedio es;%d\n",pro);
 
+if(y !=0)
+{
+    pro=sum/y;
+printf("el promedio de los números comprendidos entre 5-2500 es; %d\n",pro);
+    
+}
+else
+{
+    printf("los valores no cumplen con los requirimientos");
+}
     return 0;
 }
